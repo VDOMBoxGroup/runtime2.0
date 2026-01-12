8c7b6eb13de554880c1c4e9549c5a2a7aa9d28c3
lesf25 <dmitry.leschev@improvado.io>
2026-01-12 16:59:53 +0700

    refactor: builder CLI entry point

--- sources/startup/builder/__init__.py
+++ sources/startup/builder/__init__.py
@@ -1,19 +1,20 @@
-
-import os
-
+from pathlib import Path
 from sys import exit
+from typing import Any
+
 from argparse import ArgumentParser
 from setuptools import Extension
 
-import settings
-
-from utils.output import show, warn
+from sources import settings
+from sources.utils.output import show, warn
 from .builder import Builder, ReportBuilderFailureError
 
 EXTENSIONS = {
-    "vdomxml": Extension("memory.vdomxml._loads",
-        sources=["memory/vdomxml/loads.c"],
-        include_dirs=["memory/vdomxml/include"])
+    'vdomxml': Extension(
+        'memory.vdomxml._loads',
+        sources=['memory/vdomxml/loads.c'],
+        include_dirs=['memory/vdomxml/include'],
+    )
 }
 
 
@@ -22,54 +23,119 @@ class ArgumentsError(Exception):
 
 
 class ExceptionalArgumentParser(ArgumentParser):
-
     def error(self, message):
         raise ArgumentsError(message)
 
 
-parser = ExceptionalArgumentParser(add_help=False)
-parser.add_argument("-c", "--configure", dest="filename", default=None)
-subparsers = parser.add_subparsers(dest="action")
-subparser = subparsers.add_parser("build")
-subparsers.add_parser("deploy") #Fixed parser yielding everytime manage.py is called but without build parameters
-subparsers.add_parser("install").add_argument("applicaion.xml")
-subparser.add_argument("-l", "--list", action="store_true", dest="list", default=False,
-    help="show availavle exensions")
-subparser.add_argument("--cleanup", action="store_true", dest="cleanup", default=False,
-    help="cleanup building directories")
-subparser.add_argument("extensions", nargs="*", metavar="extension",
-    help="optional extensions to build")
+def register_build_command(subparsers: Any) -> ArgumentParser:
+    subparser = subparsers.add_parser(
+        'build',
+        help='...',
+    )
+    subparser.add_argument(
+        '-l',
+        '--list',
+        action='store_true',
+        dest='list',
+        default=False,
+        help='show availavle exensions',
+    )
+    subparser.add_argument(
+        '--cleanup',
+        action='store_true',
+        dest='cleanup',
+        default=False,
+        help='cleanup building directories',
+    )
+    subparser.add_argument(
+        'extensions',
+        nargs='*',
+        metavar='extension',
+        help='optional extensions to build',
+    )
 
-try:
-    arguments = parser.parse_args()
-except ArgumentsError as error:
-    show_warning = True
-else:
-    show_warning = False
+    return subparser
 
-    if not os.path.isdir(settings.TEMPORARY_LOCATION):
-        show("prepare temporary directory")
-        try:
-            os.makedirs(settings.TEMPORARY_LOCATION)
-        except Exception as error:
-            warn("unable to prepare temporary directory: %s" % error)
-            exit(1)
+
+def register_deploy_command(subparsers: Any) -> ArgumentParser:
+    subparser = subparsers.add_parser(
+        'deploy',
+        help='...',
+    )
+
+    return subparser
+
+
+def register_install_command(subparsers: Any) -> ArgumentParser:
+    subparser = subparsers.add_parser(
+        'install',
+        help='...',
+    )
+    subparser.add_argument('application.xml')
+
+    return subparser
+
+
+def parse_args() -> dict[str, Any]:
+    parser = ExceptionalArgumentParser(add_help=False)
+    parser.add_argument(
+        '-c',
+        '--configure',
+        dest='filename',
+        default=None
+    )
+
+    subparsers = parser.add_subparsers(dest='action')
+
+    register_build_command(subparsers)
+    register_deploy_command(subparsers)
+    register_install_command(subparsers)
+
+    return vars(parser.parse_args())
+
+
+def ensure_temp_directory() -> None:
+    temp_path = Path(settings.TEMPORARY_LOCATION)
+    if temp_path.is_dir():
+        return
+
+    show('Prepare temporary directory')
+    try:
+        temp_path.mkdir(parents=True, exist_ok=True)
+    except OSError as error:
+        warn(f'Unable to prepare temporary directory: {error}')
+        exit(1)
+
+
+def run_builder(args: dict[str, Any]) -> None:
+    builder = Builder(EXTENSIONS)
+
+    if args.get('list'):
+        builder.list()
+
+    elif args.get('cleanup'):
+        builder.cleanup()
+
+    elif extensions := args.get('extensions'):
+        builder.build(*extensions)
+
+    else:
+        builder.build()
+
+
+def main() -> None:
+    try:
+        args = parse_args()
+    except ArgumentsError:
+        exit(1)
+
+    ensure_temp_directory()
 
     try:
-        builder = Builder(EXTENSIONS)
-        if getattr(arguments, 'list'):
-            builder.list()
-        elif getattr(arguments, 'cleanup'):
-            builder.cleanup()
-        elif  getattr(arguments, 'extensions'):
-            builder.build(*arguments.extensions)
-        else:
-            builder.build()
+        run_builder(args)
     except ReportBuilderFailureError:
-        # hack
         pass
-        # exit(1)
-    except BaseException:
-        raise
-    else:
-        exit(0)
+
+
+if __name__ == '__main__':
+    main()
--- sources/startup/builder/builder.py
+++ sources/startup/builder/builder.py
@@ -1,28 +1,13 @@
-
-from builtins import str
-from builtins import object
-import sys
-import os.path
 import re
+from pathlib import Path
+from typing import Sequence
 
-from distutils.command.build_ext import build_ext
-from setuptools import setup
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-import settings
-
-from utils.output import section, show, warn
-from utils.capture import OutputCapture
-
-
-SETUP_SCRIPT = """
-setup(name="runtime",
-    cmdclass={"build_ext": BuildExtension},
-    ext_modules=extensions,
-    options={
-        "build_ext": {"build_lib": temporary, "build_temp": temporary},
-        "clean": {"build_lib": temporary, "build_temp": temporary}
-    })
-"""
+from sources import settings
+from sources.utils.output import section, show, warn
+from sources.utils.capture import OutputCapture
 
 
 class ReportBuilderFailureError(Exception):
@@ -30,73 +15,105 @@ class ReportBuilderFailureError(Exception):
 
 
 class BuildExtension(build_ext):
-
     def build_extensions(self):
-
-        def apply_options(**options):
-            options = options.get(self.compiler.compiler_type)
-            if options:
-                if isinstance(options, list):
-                    compiler_options, linker_options = options, None
-                elif isinstance(options, tuple):
-                    compiler_options, linker_options = options
-                for extension in self.extensions:
-                    if compiler_options:
-                        extension.extra_compile_args = compiler_options
-                    if linker_options:
-                        extension.extra_link_args = linker_options
-
         if settings.TREAT_WARNINGS_AS_ERRORS:
-            apply_options(msvc=["/WX"], unix=["-Werror"])
+            options = {
+                'msvc': (['/WX'], []),
+                'unix': (['-Werror'], []),
+            }
 
-        build_ext.build_extensions(self)
+            if options := options.get(self.compiler.compiler_type):
+                compiler_options, linker_options = options
+
+                for extension in self.extensions:  # type: Extension
+                    extension.extra_compile_args = compiler_options
+                    extension.extra_link_args = linker_options
+
+        super().build_extensions()
 
 
-class Builder(object):
-
-    def __init__(self, extensions):
-        self._temporary = os.path.join(settings.TEMPORARY_LOCATION, "builder")
+class Builder:
+    def __init__(
+        self,
+        extensions: dict[str, Extension]
+    ) -> None:
         self._extensions = extensions
 
-    def execute(self, *options, **keywords):
-        capture = OutputCapture("setup.log")
-        arguments_backup = sys.argv[:]
+    def execute(self, *options, **keywords) -> None:
+        capture = OutputCapture('setup.log')
         try:
-            sys.argv = ["setup.py"] + list(options)
             with capture:
-                exec(SETUP_SCRIPT, {
-                    "BuildExtension": BuildExtension,
-                    "temporary": self._temporary,
-                    "extensions": keywords.get("extensions"),
-                    "setup": setup}, {})
-        except BaseException as error:
-            show("")
-            warn(re.sub(r"^(error:\s)?", "", str(error), flags=re.IGNORECASE))
-            for line in capture.lines:
-                warn(line,
-                    indent=settings.LOGGING_INDENT,
-                    continuation=settings.LOGGING_INDENT)
-            raise ReportBuilderFailureError
-        finally:
-            sys.argv = arguments_backup
+                self._run_setup(
+                    args=options,
+                    extensions=keywords.get('extensions', []),
+                )
 
-    def list(self):
-        with section("extensions"):
+        except Exception as error:
+            error_message = re.sub(r'^(error:\s)?', '', str(error), flags=re.IGNORECASE)
+
+            show('')
+            warn(error_message)
+
+            for line in capture.lines:
+                warn(
+                    line,
+                    indent=settings.LOGGING_INDENT,
+                    continuation=settings.LOGGING_INDENT
+                )
+            raise ReportBuilderFailureError from error
+
+    def list(self) -> None:
+        with section('extensions'):
             for name in self._extensions:
                 show(name)
 
-    def cleanup(self):
-        show("cleanup build directories")
-        self.execute("clean")
+    def cleanup(self) -> None:
+        show('cleanup build directories')
+        self.execute('clean')
 
-    def build(self, *arguments):
-        with section("build extensions"):
-            for name in arguments or self._extensions:
-                try:
-                    extension = self._extensions[name]
-                except KeyError:
-                    warn("extension \"%s\" not found" % name)
+    def build(self, *arguments) -> None:
+        with section('build extensions'):
+            target_names = arguments or self._extensions.keys()
+
+            for name in target_names:
+                if (extension := self._extensions.get(name)) is None:
+                    warn(f'extension "{name}" not found')
                     raise ReportBuilderFailureError
 
-                show("build %s extension" % extension.name)
-                self.execute("build_ext", "--inplace", "clean", extensions=[extension])
+                show(f'build {extension.name} extension')
+                self.execute(
+                    'build_ext',
+                    '--inplace',
+                    'clean',
+                    extensions=[extension],
+                )
+
+    def _run_setup(
+        self,
+        args: Sequence[str],
+        extensions: 'list[Extension]',
+    ) -> None:
+        setup(
+            name='runtime',
+            cmdclass={
+                'build_ext': BuildExtension,
+            },
+            ext_modules=extensions,
+            script_args=list(args),
+            options={
+                'build_ext': {
+                    'build_lib': self._temporary,
+                    'build_temp': self._temporary,
+                },
+                'clean': {
+                    'build_lib': self._temporary,
+                    'build_temp': self._temporary,
+                }
+            }
+        )
+
+    @property
+    def _temporary(self) -> str:
+        return str(
+            Path(settings.TEMPORARY_LOCATION) / 'builder'
+        )
